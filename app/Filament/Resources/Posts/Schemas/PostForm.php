0fce64662a1eec035f36756375e9c96d9be8d5b5
babamurad2010 <babamurad2010@yandex.ru>
2025-09-12 06:41:38 +0500

    Post form

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -2,12 +2,19 @@
 
 namespace App\Filament\Resources\Posts\Schemas;
 
+use App\Models\Category;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Illuminate\Support\Str;
 
 class PostForm
 {
@@ -16,20 +23,41 @@ class PostForm
         return $schema
             ->components([
                 TextInput::make('title')
-                    ->required(),
-                TextInput::make('slug')
-                    ->required(),
-                TextInput::make('category_id')
                     ->required()
-                    ->numeric(),
-                Textarea::make('content')
+                    ->live(true)
+                    ->afterStateUpdated(function (Set $set, ?string $state, string $operation) {
+                        if ($operation === 'edit') {
+                            return;
+                        }
+                        $set('slug', Str::slug($state));
+                    }),
+                TextInput::make('slug')
+                    ->required()
+                    ->unique(ignoreRecord: true),
+                RichEditor::make('content')
                     ->columnSpanFull(),
-                FileUpload::make('image')
-                    ->image(),
+                Select::make('category_id')
+                    ->required()
+                    ->label('Select Category')
+                    ->placeholder('Select Category')
+                    ->options(Category::query()->pluck('title', 'id'))
+                    ->searchable()
+                    ->columns(1),
+                DatePicker::make('published_at')
+                    ->displayFormat('d.m.Y')
+                    ->required()
+                    ->closeOnDateSelection()
+                    ->columns(1),
                 Toggle::make('status')
+                    ->default(true)
+                    ->onIcon(Heroicon::Check)
+                    ->offIcon(Heroicon::XMark)
+                    ->onColor('success')
+                    ->offColor('danger')
+                    ->columns(1)
                     ->required(),
-                DateTimePicker::make('published_at')
-                    ->required(),
+                FileUpload::make('image')
+                    ->image()->columnSpan(2),
             ]);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'slug', 'category_id', 'content', 'image', 'status', 'published_at'];
+
+    protected static function booted()
+    {
+        static::creating(function ($category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->title);
+            }
+        });
+    }
 }
