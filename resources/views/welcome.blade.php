9bca41739a9e64afeb438a9c971136a34b9a3c04
babamurad2010 <babamurad2010@yandex.ru>
2025-09-28 05:55:00 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -299,7 +299,7 @@
 <!-- Навигация -->
 <nav id="navbar" class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#" id="logo">TurkmenTravel</a>
+        <a class="navbar-brand" href="#" id="logo">TmTourism</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
